84064b1fb8327c4efcf9f14ba6176c7de2c73905
bdnaimur <webnaimur@gmail.com>
2021-03-23 20:20:41 +0600

    data send

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
-const app = express()
+const app = express();
+const users = ["abid", 'jabe', 'asad', 'sabana']
 app.get('/', (req, res) =>{
-    res.send('hello world')
+    const fruit = {
+      name: "mango",
+      price: 300,
+      quanatity: 20
+    }
+    res.send(fruit)
   })
   
-  app.listen(3000, ()=>console.log("listening to port 3000"))
+  app.listen(3000, ()=>console.log("listening to port 3000"));
+
+  app.get('/users/:id',(req, res) =>{
+    const userId = req.params.id;
+    const name = users[userId];
+    res.send({userId, name});
+  })
+
